13049649cac52cdabbf574f8ac4fb51c19bb4ddb
wang <539959237@qq.com>
2022-03-06 15:45:42 +0800

    构建ddui项目，添加d-button组件

---
+++ ddui/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <d-button name="OK"/>
+</template>
+
+<script>
+import Button from './components/button.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'd-button':Button
+  }
+}
+</script>
+	
+<style>
+	#app{
+		margin: 20px;
+	}
+</style>
---
+++ ddui/src/components/button.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="d-button">
+    {{ name }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'd-button',
+  props: {
+    name: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.d-button{
+	padding: 0 1em;
+	height: var(--button-height);
+	font-size:var(--font-size);
+	border-radius: var(--border-radius);
+	border: 1px solid var(--border-color);
+	background: var(--button-bg);
+}
+.d-button:hover{
+	border-color: var(--border-color-hover);
+}
+.d-button:active{
+	border-color: var(--button-active-bg);
+}
+</style>
