07ea9cb0a80750935e3b0704c4ca2403a00e4dc7
wang <539959237@qq.com>
2022-05-08 12:19:39 +0800

    使用chai expect 添加四个测试用例

--- ddui/src/main.js
+++ ddui/src/main.js
@@ -18,3 +18,79 @@ new Vue({
 	  }
 	}
 }).$mount("#app")
+
+import chai from 'chai'
+const expect = chai.expect
+//单元测试
+{
+	const Constructor = Vue.extend(Button)
+	const vm = new Constructor({
+		propsData: {
+			icon: 'setting'
+		}		
+	})
+	vm.$mount()
+	let useElement = vm.$el.querySelector('use')
+	let href = useElement.getAttribute('xlink:href')
+	expect(href).to.eq('#i-setting')
+}
+{
+	const Constructor = Vue.extend(Button)
+	const vm = new Constructor({
+		propsData: {
+			icon: 'setting',
+			loading: true
+		}		
+	})
+	vm.$mount()
+	let useElement = vm.$el.querySelector('use')
+	let href = useElement.getAttribute('xlink:href')
+	expect(href).to.eq('#i-loading')
+}
+{
+	const div = document.createElement('div')
+	document.body.appendChild(div)
+	const Constructor = Vue.extend(Button)
+	const vm = new Constructor({
+		propsData: {
+			icon: 'setting'
+		}		
+	})
+	vm.$mount(div)
+	let svg = vm.$el.querySelector('svg')
+	let {order} = window.getComputedStyle(svg)
+	expect(order).to.eq('1')
+	vm.$el.remove()
+	vm.$destroy()
+}
+{
+	const div = document.createElement('div')
+	document.body.appendChild(div)
+	const Constructor = Vue.extend(Button)
+	const vm = new Constructor({
+		propsData: {
+			icon: 'setting',
+			iconPosition: 'right'
+		}		
+	})
+	vm.$mount(div)
+	let svg = vm.$el.querySelector('svg')
+	let {order} = window.getComputedStyle(svg)
+	expect(order).to.eq('2')
+	vm.$el.remove()
+	vm.$destroy()
+}
+{   // mock
+	const Constructor = Vue.extend(Button)
+	const vm = new Constructor({
+		propsData: {
+			icon: 'setting'
+		}		
+	})
+	vm.$mount()
+	vm.$on('click',function(){
+		expect(1).to.eq(1)
+	})
+	let button = vm.$el
+	button.click()
+}
