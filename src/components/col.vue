e10020bf932e52e2a39af5b152255713fc1cc688
wangtao <539959237@qq.com>
2022-06-08 18:02:20 +0800

    重构row和col

--- src/components/col.vue
+++ src/components/col.vue
@@ -1,9 +1,6 @@
 <template>
-	<div class="col" :class="[`span && col-${span}`,`offset && offset-${offset}`]"
-	:style="{paddingLeft: gutter/2+'px',paddingRight: gutter/2+'px'}">
-		<div style="border: 1px solid green;">
-			<slot></slot>
-		</div>
+	<div class="col" :class="colClass" :style="colStyle">
+		<slot></slot>
 	</div>
 </template>
 <script>
@@ -21,12 +18,26 @@
 			return {
 				gutter: 0
 			}
+		},
+		computed:{
+			colStyle(){
+				return {
+					paddingLeft: this.gutter/2+'px',
+					paddingRight: this.gutter/2+'px',
+					}
+			},
+			colClass(){
+				let {span,offset} = this
+				return [
+					span && `col-${span}`,
+					offset && `offset-${offset}`
+					]
+			}
 		}
 	}
 </script>
 <style lang="scss" scoped>
 	.col{
-		height: 100px;
 		width: 50%;
 		$class-prefix: col-;
 		@for $n from 1 through 24 {
