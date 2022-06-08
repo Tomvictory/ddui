e4cca64d5236282c98b31989c5b187ab95d01b2e
wangtao <539959237@qq.com>
2022-06-08 17:15:45 +0800

    基本实现row和col的功能

--- src/components/col.vue
+++ src/components/col.vue
@@ -1,6 +1,9 @@
 <template>
-	<div class="col" :class="[`col-${span}`]">
-		<slot></slot>
+	<div class="col" :class="[`span && col-${span}`,`offset && offset-${offset}`]"
+	:style="{paddingLeft: gutter/2+'px',paddingRight: gutter/2+'px'}">
+		<div style="border: 1px solid green;">
+			<slot></slot>
+		</div>
 	</div>
 </template>
 <script>
@@ -9,6 +12,14 @@
 		props: {
 			span:{
 				type: [Number,String]
+			},
+			offset:{
+				type: [Number,String]
+			}
+		},
+		data(){
+			return {
+				gutter: 0
 			}
 		}
 	}
@@ -16,13 +27,17 @@
 <style lang="scss" scoped>
 	.col{
 		height: 100px;
-		background: grey;
-		border: 1px solid red;
 		width: 50%;
 		$class-prefix: col-;
 		@for $n from 1 through 24 {
 			&.#{$class-prefix}#{$n} {
 				width: ($n / 24) * 100%;
+			}
+		}
+		$class-prefix: offset-;
+		@for $n from 1 through 24 {
+			&.#{$class-prefix}#{$n} {
+				margin-left: ($n / 24) * 100%;
 			}
 		}
 	}	
