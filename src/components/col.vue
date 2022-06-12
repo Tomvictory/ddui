56a4bfc13106c625248f2ac3c4be55d363f5c5fb
wangtao <539959237@qq.com>
2022-06-12 22:44:15 +0800

    稍微优化代码

--- src/components/col.vue
+++ src/components/col.vue
@@ -33,6 +33,15 @@
 				gutter: 0
 			}
 		},
+		methods:{
+			createClasses(obj,str = ''){
+				let array = []
+				if(!obj){return []}
+				if(obj.span){array.push(`col-${str}${obj.span}`)}
+				if(obj.offset){array.push(`col-${str}${obj.offset}`)}
+				return array
+			}
+		},
 		computed:{
 			colStyle(){
 				return {
@@ -42,13 +51,13 @@
 			},
 			colClass(){
 				let {span,offset,ipad,narrowPc,pc,widePc} = this
+				let createClasses = this.createClasses
 				return [
-					span && `col-${span}`,
-					offset && `offset-${offset}`,
-					... (ipad ? [`col-ipad-${ipad.span}`]:[]),
-					... (narrowPc ? [`col-narrow-pc-${narrowPc.span}`]:[]),
-					... (pc ? [`col-pc-${pc.span}`]:[]),
-					... (widePc ? [`col-wide-pc-${widePc.span}`]:[])
+					...createClasses({span,offset}),
+					...createClasses(ipad,'ipad-'),
+					...createClasses(narrowPc,'narrow-pc'),
+					...createClasses(pc,'pc-'),
+					...createClasses(widePc,'wide-pc-')
 					]
 			}
 		}
