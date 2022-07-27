d426c37e613d0e819f4e4b5d272457dcd6015a8b
wangtao <539959237@qq.com>
2022-07-27 17:04:28 +0800

    添加 tabs 相关组件，添加基本 props

---
+++ src/components/tabs-head.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="tabs-head">
+        <slot></slot>
+        <slot name="actions"></slot>
+    </div>
+</template>
+<script>
+    export default{
+        name: 'DdTabsHead'
+    }
+</script>
+<style lang="scss" scoped>
+.tabs-head{}
+</style>
--- src/main.js
+++ src/main.js
@@ -12,6 +12,11 @@ import Footer from './components/footer.vue'
 import Sider from './components/sider.vue'
 import Toast from './components/toast.vue'
 import Plugin from './plugin'
+import Tabs from './components/tabs.vue'
+import TabsHead from './components/tabs-head.vue'
+import TabsItem from './components/tabs-item.vue'
+import TabsBody from './components/tabs-body.vue'
+import TabsPane from './components/tabs-pane.vue'
 Vue.use(Plugin)
 Vue.component('d-toast', Toast)
 Vue.component('d-icon', Icon)
@@ -25,13 +30,19 @@ Vue.component('d-header',Header)
 Vue.component('d-content',Content)
 Vue.component('d-footer',Footer)
 Vue.component('d-sider',Sider)
+Vue.component('d-tabs',Tabs)
+Vue.component('d-tabs-head',TabsHead)
+Vue.component('d-tabs-item',TabsItem)
+Vue.component('d-tabs-body',TabsBody)
+Vue.component('d-tabs-pane',TabsPane)
 
 new Vue({
 	data:{
 		loading1: false,
 		loading2: false,
 		loading3: false,
-		inputValue: 'test'
+		inputValue: 'test',
+		selectedTab: 'sports'
 	},
 	methods:{
 		showToast1:function(){
